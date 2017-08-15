39788812257c4cc0ffbac6635972f6948ffaf3f8
USER <user@shushu.local>
2017-08-15 17:53:37 +0900

    asset

--- db/migrate/20170814183049_add_confirmable_to_devise.rb
+++ db/migrate/20170814183049_add_confirmable_to_devise.rb
@@ -5,13 +5,12 @@ class AddConfirmableToDevise < ActiveRecord::Migration
     add_column :users, :confirmation_sent_at, :datetime
     add_column :users, :unconfirmed_email, :string
     add_index :users, :confirmation_token, unique: true
-    # User.reset_column_information # Need for some types of updates, but not for update_all.
 
     execute("UPDATE users SET confirmed_at = NOW()")
   end
 
   def down
     remove_columns :users, :confirmation_token, :confirmed_at, :confirmation_sent_at
-    remove_columns :users, :unconfirmed_email # Only if using reconfirmable
+    remove_columns :users, :unconfirmed_email
   end
 end
